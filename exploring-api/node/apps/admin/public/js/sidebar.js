3765abde74acc79a59261def454510389d16dffe
PauloCesar26 <pc.cordeirolima@gmail.com>
2026-04-20 10:37:10 -0300

    Adding rotate to the dropdown icon

--- exploring-api/node/apps/admin/public/js/sidebar.js
+++ exploring-api/node/apps/admin/public/js/sidebar.js
@@ -1,19 +1,20 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM carregado");
-
   const dropdowns = document.querySelectorAll(".dropdown");
-
+  
+  console.log("DOM carregado");
   console.log("Dropdowns encontrados:", dropdowns.length);
 
   dropdowns.forEach((dropdown) => {
     const button = dropdown.querySelector(".dropdown-button");
     const menu = dropdown.querySelector(".dropdown-menu");
+    const icon = dropdown.querySelector(".dropdown-icon");
 
     console.log(button, menu);
     button.addEventListener("click", () => {
-    console.log("clicou");
-    menu.classList.toggle("hidden");
+      console.log("clicou");
+      menu.classList.toggle("hidden");
+      icon.classList.toggle("rotate-180");
     });
   });
 });
